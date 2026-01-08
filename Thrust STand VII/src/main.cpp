f2ad6d4d61beaaf54847d3836557f74ea0eafb0a
DBF-UW <126025754+DBF-UW@users.noreply.github.com>
2026-01-07 20:00:30 -0800

    Added loading screen and keypad support

--- Thrust STand VII/src/main.cpp
+++ Thrust STand VII/src/main.cpp
@@ -1,6 +1,53 @@
 #include <Arduino.h>
+#include <Keypad.h>
 #include <U8g2lib.h>
 #include <Wire.h>
+
+//-----------------------------------------GLOBAL VARIABLES-----------------------------------
+
+//test setup
+int rpmMarkers = 1;
+int testNumber = 1;
+//smooth ramp
+int rampTime = 30; //in seconds
+int topTime = 4; //in seconds
+int smoothThrottleMax = 100; //as a percent
+bool upDown = true; //if true, go up and then back down
+
+
+//---------------------------------------KEYBOARD SETUP---------------------------------------
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+
+// Define the key mapping based on your keypad's layout
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+// Connect the row pins to the Arduino digital pins
+byte rowPins[ROWS] = {9, 8, 7, 6}; // Adjust these if your wiring is different
+
+// Connect the column pins to the Arduino digital pins
+byte colPins[COLS] = {5, 4, 3, 2}; // Adjust these if your wiring is different
+
+// Create the Keypad object
+Keypad customKeypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS)
+
+
+//---------------------------------------SCREEN AND MENU--------------------------------------
+//this is the magic that makes the screen work. Screen needs to be hooked up to SDA and SCL
+// SSD1309, 128x64, I2C
+U8G2_SSD1309_128X64_NONAME0_F_HW_I2C u8g2(
+    U8G2_R0,
+    U8X8_PIN_NONE
+);
+
+//UW Logo
+static const unsigned char image_download_bits[] = {0xa0,0x00,0x00,0x00,0x00,0x80,0x07,0x00,0x00,0x00,0x04,0x3e,0x00,0x00,0x00,0xf8,0xf8,0x00,0x00,0x00,0xc0,0xe3,0x43,0x01,0x00,0x0f,0xdf,0xff,0x03,0x00,0x7c,0xfc,0xff,0x01,0x00,0xe0,0xf9,0xff,0x01,0x00,0x80,0xef,0x7f,0x02,0x00,0x00,0xff,0x3f,0x00,0x00,0x00,0xfe,0x7f,0x40,0x00,0x00,0xfc,0x7f,0x80,0x00,0x00,0xf8,0x07,0x04,0x02,0x00,0x20,0x08,0x10,0x04,0x00,0x00,0x00,0x40,0x08,0x00,0x00,0x00,0x82,0x10,0x00,0x00,0x00,0x04,0x21,0x00,0x00,0x00,0x10,0x22,0x00,0x00,0x00,0x20,0x24,0x00,0x00,0x00,0x40,0x0c,0x00,0x00,0x00,0xc0,0x07,0x00,0x00,0x00,0x00,0x02,0x00,0x00,0x00,0xa0,0x00,0x00,0x00,0xaa,0x8a,0x00,0x00,0x00,0x00,0x30,0x00,0x00,0x00,0xd0,0x03,0x00};
+
 
 //-----------------------------------------MENU FUNCTIONS--------------------------------------
 /*
@@ -12,41 +59,35 @@ MENU FLOW CHART:
         Test Data Readout (of all sensors)
 
     2 Configure Test
-        21 Test Profile Selection
-            211 Smooth Up/Down
-                2111 Total Time
-                2112 Time at Top
-                2113 Max Throttle
-                2114 Up/Down Mode (Just up or up and down)
-            212 Discreet Intervals
-                2121 Interval Count
-                2122 Interval Time
-                2123 Max Throttle
-                2124 Up/Down Mode (Just up or up and down)
-        22 Test Setup Selection
-            221 RPM Marker Count
-            222 Test File Name
+        21 Test Profile Selection - Action
+        22 Test Profile Configuration
+            221 Smooth Up/Down
+                2211 Ramp Time
+                2212 Time at Top
+                2213 Max Throttle
+                2214 Up/Down Mode (Just up or up and down)
+            222 Discreet Intervals
+                2221 Interval Count
+                2222 Interval Time
+                2223 Max Throttle
+                2224 Up/Down Mode (Just up or up and down)
+        23 Test Setup Selection
+            231 RPM Marker Count
+            232 Test File Name
 
     3 Tare Sensors
-        // Tare Torque
-        // Tare Force
-        // Zero All
-        // Zero Force
-        // Zero Torque
-        // Zero Airspeed
-        // Zero Current
-        // Zero Voltage
+        // 31 Zero All
+        // 32 Tare Torque
+        // 33 Tare Force
+        // 34 Zero Force
+        // 35 Zero Torque
+        // 36 Zero Analog
 
     4 Debug Menu
         Display read values for all sensors,
 */
 
-//this is the magic that makes the screen work. Screen needs to be hooked up to SDA and SCL
-// SSD1309, 128x64, I2C
-U8G2_SSD1309_128X64_NONAME0_F_HW_I2C u8g2(
-    U8G2_R0,
-    U8X8_PIN_NONE
-);
+
 
 enum ItemType { TYPE_SUBMENU, TYPE_TOGGLE, TYPE_VALUE, TYPE_ACTION };
 
@@ -63,17 +104,34 @@ struct MenuItem {
 MenuItem menus[] = {
     //MAIN MENU
     {0, "Main Menu", TYPE_SUBMENU, 1000, NULL, NULL},
+
     {1, "Run Test", TYPE_ACTION, 0, NULL, NULL},
+
     {2, "Configure Test", TYPE_SUBMENU, 0, NULL, NULL},
+        {21, "Select Test Profile", TYPE_ACTION, 2, NULL, NULL},
+        {22, "Configure Test Profiles", TYPE_SUBMENU, 2, NULL, NULL},
+            {221, "Smooth Ramp", TYPE_SUBMENU, 22, NULL, NULL},
+                {2211, "Ramp Time", TYPE_VALUE, 22, &rampTime, NULL},
+                {2212, "Top Time", TYPE_VALUE, 22,},
+            {222, "Interval Ramp", TYPE_SUBMENU, 22, NULL, NULL},
+        {23, "Test Setup Selection", TYPE_SUBMENU, 2, NULL, NULL},
+            {231, "RPM Marker Count", TYPE_VALUE, 23, NULL, NULL},
+
     {3, "Tare Sensors", TYPE_SUBMENU, 0, NULL, NULL},
+        {31, "Zero All", TYPE_ACTION, 3, NULL, NULL},
+        {32, "Tare Torque", TYPE_ACTION, 3, NULL, NULL},
+        {33, "Tare Thrust", TYPE_ACTION, 3, NULL, NULL},
+        {34, "Zero Force", TYPE_ACTION, 3, NULL, NULL},
+        {35, "Zero Torque", TYPE_ACTION, 3, NULL, NULL},
+        {36, "Zero Analog", TYPE_ACTION, 3, NULL, NULL},
+
     {4, "Debug", TYPE_ACTION, 0, NULL, NULL},
 
     //TARE SENSORS
-    {31, "Zero All", TYPE_ACTION, 3, NULL, NULL},
-    {32, "Tare Torque", TYPE_ACTION, 3, NULL, NULL},
-    {33, "Tare Thrust", TYPE_ACTION, 3, NULL, NULL},
-    {34, "Zero Forces", TYPE_ACTION, 3, NULL, NULL},
-    {35, "Zero Analog", TYPE_ACTION, 3, NULL, NULL},
+
+
+    //Configure Test
+
 }; 
 
 //this has to exist because calculating the number of items
@@ -82,6 +140,8 @@ int menuCount = sizeof(menus)/sizeof(menus[0]);
 int currentMenuId = 0; //this keeps track of the current menu state
 //returns null if no Menu Item with that ID, otherwise returns a pointer
 //to the item.
+
+
 MenuItem* getMenu(int menuId) {
     for (int i = 0; i < menuCount; i++) {
         if (menus[i].itemId == menuId) {
@@ -90,6 +150,7 @@ MenuItem* getMenu(int menuId) {
     }
     return nullptr; //returns null if there's no menu with that ID
 }
+
 
 void drawMenu(int menuId) {
     u8g2.clearBuffer(); //prepare the screen for writing
@@ -119,7 +180,7 @@ void drawMenu(int menuId) {
     u8g2.sendBuffer();
 }
 
-int getChosenMenuId(int choice) { //given an the int of the choice (1 index), then it will find the chosen menu Id. If the choice is invalid, then returns null
+int getChosenMenuId(int choice) { //given an the int of the choice (1 index), then it will find the chosen menu Id. If the choice is invalid, then returns -1
 
     //fetch that menus pointer
     for (int i = 0; i < menuCount; i++) {
@@ -127,12 +188,13 @@ int getChosenMenuId(int choice) { //given an the int of the choice (1 index), th
             if (choice == 1) {
                 return menus[i].itemId;
             } else if (choice < 1){
-                return NULL;
+                return -1;
             } else {
                 choice = choice - 1;
             }
         }
     }
+    return -1;
 }
 
 void executeMenu(int targetMenuId) {
@@ -148,8 +210,35 @@ void executeMenu(int targetMenuId) {
     }
 }
 
+//pass load percent as an int from 0-100
+void drawLoadingScreen(int loadPercent){
+    u8g2.clearBuffer();
+
+    u8g2.drawXBM(85, 25, 38, 26, image_download_bits);
+
+    u8g2.setFont(u8g2_font_t0_13b_tr);
+    u8g2.drawStr(3, 14, "Design Build Fly");
+
+    u8g2.setFont(u8g2_font_4x6_tr);
+    u8g2.drawStr(3, 22, "At the University of Washington");
+
+    u8g2.drawStr(90, 60, "2025-26");
+
+    u8g2.drawRFrame(2, 30, 80, 21, 3); //loading bar frame
+
+    u8g2.drawRBox(2, 30, ((80-6)*loadPercent/100)+6, 21, 3); //loading bar fill in
+
+    u8g2.drawStr(2, 60, "Version 0.1");
+
+    u8g2.sendBuffer();
+
+}
+
 void setup() {
     u8g2.begin();
+    for (int i = 0; i < 20; i++){
+        drawLoadingScreen(i*5);
+    }
 }
 
 
@@ -157,9 +246,20 @@ void setup() {
 //If you would like your function to return to the main menu after completing, set the currentMenuId to zero at the end of your function runs
 void loop() { 
     drawMenu(currentMenuId);
-    int choice = 3; //get keypad input here
+    delay(5000);
+    drawLoadingScreen(0);
+    delay(5000);
+    //executeMenu(3);
+    /*int choice = 3; //get keypad input here
     int chosenId = getChosenMenuId(choice);
     if (chosenId) {
         executeMenu(chosenId);
-    }
+    }*/
+    
+    /*
+    u8g2.clearBuffer(); //prepare the screen for writing
+    u8g2.setFont(u8g2_font_6x12_tr); //set big font for parent menu
+    u8g2.drawLine(0, 10, 128, 10); //draw line across bottom
+    u8g2.sendBuffer();
+    */
 }
